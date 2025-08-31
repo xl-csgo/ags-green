83bf808042a02a51fde189ee114669b3efd5c870
tctg0arnav <59003715+tctg0arnav@users.noreply.github.com>
2025-08-31 22:43:15 +0530

    gsap introduced

--- src/components/Offerings.js
+++ src/components/Offerings.js
@@ -24,51 +24,7 @@ const Offerings = () => (
 			<a className="learn-btn" href="/#learn-more">Learn More</a>
 			</div>
 			<div className="offering-illustration">
-				<svg width="500" height="813" viewBox="0 0 427 813" fill="none" xmlns="http://www.w3.org/2000/svg">
-					{/* Outer glows pulsate slowly */}
-					<ellipse opacity="0.15" cx="405.5" cy="406.5" rx="405.5" ry="405.5" fill="url(#paint0_linear_2016_99)">
-						<animate attributeName="rx" values="405.5;425;405.5" dur="6s" repeatCount="indefinite" />
-						<animate attributeName="ry" values="405.5;430;405.5" dur="6s" repeatCount="indefinite" />
-					</ellipse>
-					<ellipse opacity="0.25" cx="405.5" cy="406.5" rx="330.5" ry="331.5" fill="url(#paint1_linear_2016_99)">
-						<animate attributeName="rx" values="330.5;345;330.5" dur="6s" repeatCount="indefinite" />
-						<animate attributeName="ry" values="331.5;346;331.5" dur="6s" repeatCount="indefinite" />
-					</ellipse>
-					<ellipse opacity="0.5" cx="424.5" cy="406.5" rx="271.5" ry="272.5" fill="url(#paint2_linear_2016_99)">
-						<animate attributeName="rx" values="271.5;285;271.5" dur="6s" repeatCount="indefinite" />
-						<animate attributeName="ry" values="272.5;286;272.5" dur="6s" repeatCount="indefinite" />
-					</ellipse>
-					<ellipse opacity="0.7" cx="434.5" cy="406.5" rx="215.5" ry="216.5" fill="url(#paint3_linear_2016_99)">
-						<animate attributeName="rx" values="215.5;228;215.5" dur="6s" repeatCount="indefinite" />
-						<animate attributeName="ry" values="216.5;229;216.5" dur="6s" repeatCount="indefinite" />
-					</ellipse>
-					{/* Core circle pulses slightly faster */}
-					<circle cx="454" cy="406" r="152" fill="url(#paint4_linear_2016_99)">
-						<animate attributeName="r" values="152;166;152" dur="6s" repeatCount="indefinite" />
-					</circle>
-					<defs>
-					<linearGradient id="paint0_linear_2016_99" x1="405.5" y1="0" x2="405.5" y2="813" gradientUnits="userSpaceOnUse">
-					<stop stop-color="#FFBE0C"/>
-					<stop offset="1" stop-color="#FF9A24"/>
-					</linearGradient>
-					<linearGradient id="paint1_linear_2016_99" x1="405.5" y1="75" x2="405.5" y2="738" gradientUnits="userSpaceOnUse">
-					<stop stop-color="#FFBE0C"/>
-					<stop offset="1" stop-color="#FF9A24"/>
-					</linearGradient>
-					<linearGradient id="paint2_linear_2016_99" x1="424.5" y1="134" x2="424.5" y2="679" gradientUnits="userSpaceOnUse">
-					<stop stop-color="#FFBE0C"/>
-					<stop offset="1" stop-color="#FF9A24"/>
-					</linearGradient>
-					<linearGradient id="paint3_linear_2016_99" x1="434.5" y1="190" x2="434.5" y2="623" gradientUnits="userSpaceOnUse">
-					<stop stop-color="#FFBE0C"/>
-					<stop offset="1" stop-color="#FF9A24"/>
-					</linearGradient>
-					<linearGradient id="paint4_linear_2016_99" x1="454" y1="254" x2="454" y2="558" gradientUnits="userSpaceOnUse">
-					<stop stop-color="#FFBE0C"/>
-					<stop offset="1" stop-color="#FF9A24"/>
-					</linearGradient>
-					</defs>
-				</svg>
+				<img src={sunSvg} alt="Illustration of the Sun." />
 			</div>
 			
 		</div>
